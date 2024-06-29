e4c07b0bb3cd9ca3990e12d1dcdc3914e3bea3f8
caueeugenio <caue_eugenio@hotmail.com>
2024-06-29 19:43:41 -0300

    initial commit

---
+++ src/env/index.ts
@@ -0,0 +1,20 @@
+import "dotenv/config";
+
+import { z } from "zod";
+
+const envSchema = z.object({
+  NODE_ENV: z
+    .enum(["development", "production", "test"])
+    .default("development"),
+  PORT: z.coerce.number().default(3000),
+  //   coerce faz conversáo de um number para string
+});
+
+const _env = envSchema.safeParse(process.env);
+
+if (!_env.success) {
+  console.log("Invalid environment variables", _env.error.format());
+  throw new Error("Invalid environment variables.");
+}
+
+export const env = _env.data;
