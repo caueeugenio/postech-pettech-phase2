30b4e5e274d4ffc9c888b29bfb3b40b2c5cb108c
caueeugenio <caue_eugenio@hotmail.com>
2024-07-07 21:37:11 -0300

    updating branch

---
+++ src/http/controllers/person/create.ts
@@ -0,0 +1,25 @@
+import { PersonRepository } from '@/repositories/person.repository'
+import { CreatePersonUseCase } from '@/use-cases/create-person'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function create(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    cpf: z.string(),
+    name: z.string(),
+    birth: z.date(),
+    email: z.string().email(),
+  })
+
+  const { cpf, name, birth, email } = registerBodySchema.parse(request.body)
+
+  try {
+    const personRepository = new PersonRepository()
+    const createPersonUseCase = new CreatePersonUseCase(personRepository)
+    await createPersonUseCase.handler({ cpf, name, birth, email })
+    return reply.status(201).send()
+  } catch (error) {
+    console.log(error)
+    throw new Error('Internal server error')
+  }
+}
