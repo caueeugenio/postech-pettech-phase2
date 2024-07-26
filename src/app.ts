cfec045a0ead698af794ec58ccb1f27a1c6d3da1
caueeugenio <caue_eugenio@hotmail.com>
2024-07-25 22:13:25 -0300

    updating branch

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
+import 'reflect-metadata'
+import '@/lib/typeorm/typeorm'
 import fastify from 'fastify'
 import { personRoutes } from '@/http/controllers/person/routes'
 import { userRoutes } from './http/controllers/user/routes'
 import { globalErrorHandler } from './utils/global-error-handler'
 import { addressRoutes } from './http/controllers/address/routes'
-// estamos inicializando o fastify
-export const app = fastify()
+
+export const app = fastify() // estamos inicializando o fastify
 app.register(personRoutes)
 app.register(userRoutes)
 app.register(addressRoutes)
---
+++ src/lib/typeorm/typeorm.ts
@@ -0,0 +1,24 @@
+import { DataSource } from 'typeorm'
+import { env } from '@/env'
+import { Product } from '@/entities/product.entity'
+import { Category } from '@/entities/category.entity'
+
+export const appDataSource = new DataSource({
+  type: 'postgres',
+  host: env.DATABASE_HOST,
+  port: env.DATABASE_PORT,
+  username: env.DATABASE_USER,
+  password: env.DATABASE_PASSWORD,
+  database: env.DATABASE_NAME,
+  entities: [Product, Category],
+  logging: env.NODE_ENV === 'development',
+})
+
+appDataSource
+  .initialize()
+  .then(() => {
+    console.log('Database with typeORM connected')
+  })
+  .catch((error) => {
+    console.error('Error connecting to database with typeORM', error)
+  })
